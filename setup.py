16fc9e3726946b874d3cac6d7262d7ba6d719a89
Fredrik Nygård Carlsen <me@frecar.no>
2013-11-29 22:34:45 +0100

    update version to 0.1.5

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.settings'
 
 setup(
     name="django-basis",
-    version='0.1.3',
+    version='0.1.5',
     url='http://github.com/frecar/django-basis',
     author='Fredrik Nygård Carlsen',
     author_email='me@frecar.no',
