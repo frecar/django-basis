550f7b54913cd663a2bc245502cbfa6d2c6dc4bc
ekmartin <ekmartinb@gmail.com>
2014-11-05 22:47:24 +0100

    Move the persistent stuff into PersistentModel and update the tests accordingly

--- basis/managers.py
+++ basis/managers.py
@@ -2,20 +2,20 @@ from django.db import models
 from .compat import DJANGO16
 
 
-class _BasisModelManager(models.Manager):
+class _PersistentModelManager(models.Manager):
     def create(self, *args, **kwargs):
         user = kwargs.pop('current_user', None)
-        instance = super(_BasisModelManager, self).create(*args, **kwargs)
+        instance = super(_PersistentModelManager, self).create(*args, **kwargs)
         instance.created_by = user
         instance.updated_by = user
         return instance
 
 
 if DJANGO16:
-    class BasisModelManager(_BasisModelManager):
+    class PersistentModelManager(_PersistentModelManager):
         def get_queryset(self):
-            return super(BasisModelManager, self).get_queryset().filter(deleted=False)
+            return super(PersistentModelManager, self).get_queryset().filter(deleted=False)
 else:
-    class BasisModelManager(_BasisModelManager):
+    class PersistentModelManager(_PersistentModelManager):
         def get_query_set(self):
-            return super(BasisModelManager, self).get_query_set().filter(deleted=False)
+            return super(PersistentModelManager, self).get_query_set().filter(deleted=False)
